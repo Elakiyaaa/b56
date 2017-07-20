5147b6caba4fc6d9f1d6fa7e01840f8ca6236ec8
Elakiyaaa <elakthamizh@gmail.com>
2017-07-20 12:05:52 +0530

    Add files via upload

---
+++ B60.java
@@ -0,0 +1,37 @@
+package GUVI;
+import java.util.Scanner;
+public class B60 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a[]=new int[n];
+		int i;
+		int min=1;
+		int max=a[0];
+		for( i=0;i<a.length;i++){
+			a[i]=s.nextInt();
+		}
+		for( i=0;i<a.length;i++){
+			if(a[i]>=max){
+			max=a[i];
+			}
+			if(a[i]<=min){
+					min=a[i];
+						}
+}
+		
+		System.out.println(min);
+		System.out.println(max);
+}
+	/*	for( i=0;i<n;i++){
+			for(int j=i+1;j<n;j++){
+				
+			}
+		}*/
+		
+
+	}
+
+
