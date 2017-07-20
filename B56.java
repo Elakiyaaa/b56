28a455778b408c2d89b35503facf71ae90b24cd4
Elakiyaaa <elakthamizh@gmail.com>
2017-07-20 11:22:19 +0530

    Add files via upload

---
+++ B56.java
@@ -0,0 +1,13 @@
+package GUVI;
+import java.util.Scanner;
+public class B56 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  Scanner s=new Scanner(System.in);
+		  String a=s.nextLine();
+		  char ch=s.next().charAt(0);
+		  System.out.println(a+","+ch);
+	}
+
+}
